cec2ba1744c11a78bfd34dfc681144c5017eefb7
Shihab <saljahwa@kent.edu>
2023-04-07 19:24:26 -0400

    Updated sketch.js

--- soccer-web-toy/sketch.js
+++ soccer-web-toy/sketch.js
@@ -1,4 +1,3 @@
-// Set up the soccer ball and goal
 let ballX, ballY, ballSize = 50;
 let goalX, goalY, goalW = 200, goalH = 100;
 let dragging = false;
@@ -13,16 +12,13 @@ function setup() {
 }
 
 function draw() {
-  // Change the background color to green (R, G, B)
   background(0, 200, 0);
 
-  // Draw the goal with a white border and transparent fill
   noFill();
   stroke(255);
   strokeWeight(4);
   rect(goalX, goalY, goalW, goalH);
 
-  // Draw black lines as a net inside the goal (both vertical and horizontal)
   stroke(0);
   strokeWeight(2);
   for (let i = 1; i <= 4; i++) {
@@ -30,13 +26,11 @@ function draw() {
     line(goalX, goalY + i * (goalH / 5), goalX + goalW, goalY + i * (goalH / 5));
   }
 
-  // If dragging, move the ball
   if (dragging) {
     ballX = mouseX;
     ballY = mouseY;
   }
 
-  // Draw the soccer ball with a white base and black patches
   fill(255);
   stroke(0);
   strokeWeight(4);
@@ -53,10 +47,8 @@ function draw() {
   textSize(24);
   textAlign(CENTER, CENTER);
   
-  // Change the text color to a lighter gray (R, G, B)
   fill(200, 200, 200);
 
-  // Set the text font type to a more readable cursive font
   textFont("Comic Sans MS");
 
   text("Drag the ball into the goal!", width / 2, 40);
@@ -64,14 +56,12 @@ function draw() {
 }
 
 
-// Check if the ball is being dragged
 function mousePressed() {
   if (dist(ballX, ballY, mouseX, mouseY) < ballSize / 2) {
     dragging = true;
   }
 }
 
-// When the mouse is released, check if the ball is in the goal
 function mouseReleased() {
   dragging = false;
   if (ballX > goalX && ballX < goalX + goalW && ballY > goalY && ballY < goalY + goalH) {
